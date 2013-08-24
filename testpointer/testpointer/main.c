e6ff692b7dba61019e481dc45e7a8d8c64be08d0
takatsugu ishikawa <tissi0708@gmail.com>
2013-08-24 23:24:20 +0900

    add a part of using array

--- testpointer/testpointer/main.c
+++ testpointer/testpointer/main.c
@@ -7,13 +7,44 @@
 //
 
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+char paragraph[] = "In computing, C is a general-purpose programming language initially developed by Dennis Ritchie between 1969 and 1973 at AT&T Bell Labs. Like most imperative languages in the ALGOL tradition, C has facilities for structured programming and allows lexical variable scope and recursion, while a static type system prevents many unintended operations. Its design provides constructs that map efficiently to typical machine instructions, and therefore it has found lasting use in applications that had formerly been coded in assembly language, most notably system software like the Unix computer operating system.";
+
+char words[100][20];
+int words_count = 0;
 
 int main(int argc, const char * argv[])
 {
-
     int i = 0;
+    int word_size = 0;
+    char c;
+    
+    char test[3][25];
+    strcpy (test[0], "石川たかつぐ");
+    printf("Hello! %s\n", test[0]);
+    
+    while ((c = paragraph[i]) != '\0') {
+        if (isalpha(c)) {
+            words[words_count][word_size] = c;
+            word_size++;
+        } else if (isspace(c) && word_size > 0) {
+            words[words_count][word_size] = '\0';
+            word_size = 0;
+            words_count++;
+        }
+        else {
+            
+        }
+        i++;
+    }
+    
     // insert code here...
-    printf("Hello, World!\n");
+    for (i = 0; i < words_count; i++) {
+        printf("%s\n", words[i]);
+    }
+
     return 0;
 }
 
