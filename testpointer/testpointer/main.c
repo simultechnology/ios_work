6c3612b0824d8dfc96a05d7d3b482cf4172ba8fb
takatsugu ishikawa <tissi0708@gmail.com>
2013-08-24 23:57:21 +0900

    add pointer sample

--- testpointer/testpointer/main.c
+++ testpointer/testpointer/main.c
@@ -10,41 +10,37 @@
 #include <ctype.h>
 #include <string.h>
 
-char paragraph[] = "In computing, C is a general-purpose programming language initially developed by Dennis Ritchie between 1969 and 1973 at AT&T Bell Labs. Like most imperative languages in the ALGOL tradition, C has facilities for structured programming and allows lexical variable scope and recursion, while a static type system prevents many unintended operations. Its design provides constructs that map efficiently to typical machine instructions, and therefore it has found lasting use in applications that had formerly been coded in assembly language, most notably system software like the Unix computer operating system.";
+char paragraph[] = "In computing, C is a general-purpose programming language initially developed by Dennis Ritchie between 1969 and 1973 at AT&T Bell Labs. Like most imperative languages in the ALGOL tradition, C has facilities for structured programming and allows lexical variable scope and recursion, while a static type system prevents many unintended operations. Its design provides constructs that map efficiently to typical machine instructions, and therefore it has found lasting use in applications that had formerly been coded in assembly language, most notably system software like the Unix computer operating a koko kokoko kokookoko system. ";
 
-char words[100][20];
+char *words[100];
 int words_count = 0;
+char word_buffer[1024];
+int word_buffer_end = 0;
 
 int main(int argc, const char * argv[])
 {
+    int word_buffer_top = 0;
     int i = 0;
-    int word_size = 0;
     char c;
     
-    char test[3][25];
-    strcpy (test[0], "石川たかつぐ");
-    printf("Hello! %s\n", test[0]);
-    
-    while ((c = paragraph[i]) != '\0') {
+    while ((c = paragraph[i++]) != '\0') {
         if (isalpha(c)) {
-            words[words_count][word_size] = c;
-            word_size++;
-        } else if (isspace(c) && word_size > 0) {
-            words[words_count][word_size] = '\0';
-            word_size = 0;
-            words_count++;
+            word_buffer[word_buffer_end++] = c;
+        } else if (isspace(c) && word_buffer_top != word_buffer_end) {
+            word_buffer[word_buffer_end++] = '\0';
+            words[words_count++] = &word_buffer[word_buffer_top];
+            word_buffer_top = word_buffer_end;
         }
         else {
             
         }
-        i++;
     }
     
     // insert code here...
     for (i = 0; i < words_count; i++) {
         printf("%s\n", words[i]);
     }
-
+    printf("total word_count : %d\n", words_count);
     return 0;
 }
 
---
+++ testpointer/testpointer/notmain.h
@@ -0,0 +1,14 @@
+//
+//  notmain.h
+//  testpointer
+//
+//  Created by ishi on 2013/08/24.
+//  Copyright (c) 2013年 simultechnology. All rights reserved.
+//
+
+#ifndef testpointer_notmain_h
+#define testpointer_notmain_h
+
+
+
+#endif
